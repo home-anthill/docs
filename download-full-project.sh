fd4f95d9bbf245e67d85840e8d984d8781247e36
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2024-08-30 02:05:41 +0200

    feat: update local-development doc to deploy a mongodb cluster on your dev machine
    Signed-off-by: Stefano Cappa <stefano.cappa.ks89@gmail.com>

--- download-full-project.sh
+++ download-full-project.sh
@@ -15,6 +15,7 @@ git clone https://github.com/home-anthill/mosquitto.git
 git clone https://github.com/home-anthill/devices.git
 git clone https://github.com/home-anthill/sensors.git
 git clone https://github.com/home-anthill/deployer.git
+git clone https://github.com/home-anthill/sharded-mongodb-docker.git
 
 # always from the `home-anthill` folder created above:
 # create a folder where you can put your custom configuration
